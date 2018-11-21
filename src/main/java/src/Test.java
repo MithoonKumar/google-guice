6e9bf2e25ba5e9c0e5153e3f89a8a25486f4a391
Mithoon Kumar <mithoonk@zeta.tech>
2018-11-21 13:59:23 +0530

    Yet another test commit

--- src/main/java/src/Test.java
+++ src/main/java/src/Test.java
@@ -28,6 +28,6 @@ public class Test {
        Injector injector = Guice.createInjector(new BinderModule());
        Utility utility = injector.getInstance(Utility.class);
        utility.callMethod();
-       System.out.println("Another Test Commit");
+       System.out.println("Testing Again");
     }
 }
