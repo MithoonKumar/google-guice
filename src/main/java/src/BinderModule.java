c4392ec5922740dbbfac8f26e8d2c57ae3bb3a3d
Mithoon Kumar <mithoonk@zeta.tech>
2018-11-21 14:29:08 +0530

    Named annotation

--- src/main/java/src/BinderModule.java
+++ src/main/java/src/BinderModule.java
@@ -2,13 +2,14 @@ package src;
 
 
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
 
 public class BinderModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(TestInterface.class).annotatedWith(CustomAnnotation1.class).to(SubClasstTestInterfaceImpl1.class);
-        bind(TestInterface.class).annotatedWith(CustomAnnotation2.class).to(SubClasstTestInterfaceImpl2.class);
+        bind(TestInterface.class).annotatedWith(Names.named("firstImpl")).to(SubClasstTestInterfaceImpl1.class);
+        bind(TestInterface.class).annotatedWith(Names.named("secondImpl")).to(SubClasstTestInterfaceImpl2.class);
     }
 }
 
--- src/main/java/src/Utility.java
+++ src/main/java/src/Utility.java
@@ -1,12 +1,14 @@
 package src;
 
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
 
 public class Utility {
     private TestInterface testInterface1;
 
     @Inject
-    public Utility(@CustomAnnotation2 TestInterface testInterface2) {
+    public Utility(@Named("secondImpl") TestInterface testInterface2) {
         this.testInterface1 = testInterface2;
     }
 
