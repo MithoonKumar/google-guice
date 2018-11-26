88df3d4e9d3db7893004fb4252b41d24197053b8
Mithoon Kumar <mithoonk@zeta.tech>
2018-11-26 11:50:17 +0530

    Add some comments

--- src/main/java/src/ImplementedBy/TestInterface.java
+++ src/main/java/src/ImplementedBy/TestInterface.java
@@ -2,7 +2,7 @@ package src.ImplementedBy;
 
 import com.google.inject.ImplementedBy;
 
-@ImplementedBy(TestInterfaceImpl.class)
+@ImplementedBy(TestInterfaceImpl.class) // The @ImplementedBy has less priority as compared to the Guice module binding in the binder class.
 public interface TestInterface {
     void print();
 }
