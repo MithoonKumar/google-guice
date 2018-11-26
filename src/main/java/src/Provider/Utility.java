7623e46b620725dae3b9162279a27e7a78f3b4fe
Mithoon Kumar <mithoonk@zeta.tech>
2018-11-26 14:31:40 +0530

    Provider interface without using .get

--- src/main/java/src/Provider/Utility.java
+++ src/main/java/src/Provider/Utility.java
@@ -5,15 +5,21 @@ import com.google.inject.Provider;
 
 public class Utility {
 
-    private Provider<TestInterface> testInterface;
+    //private Provider<TestInterface> testInterface;
+    private TestInterface testInterface;
+
+//    @Inject
+//    public Utility(Provider<TestInterface> testInterface) {
+//        this.testInterface = testInterface;
+//    }
 
     @Inject
-    public Utility(Provider<TestInterface> testInterface) {
+    public Utility(TestInterface testInterface) {
         this.testInterface = testInterface;
     }
 
     public void doSomething() {
-        int val = testInterface.get().someVal();
+        int val = testInterface.someVal();
         System.out.println("Printing the value" + val);
     }
 }
